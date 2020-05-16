f07a9021a886c5ce50b92ea8a2469a410da24d6e
Areez Visram <visraa7@mcmaster.ca>
2020-05-16 12:52:37 -0400

    done questionnaire

--- static/main.js
+++ static/main.js
@@ -3,21 +3,37 @@ document.getElementById("quiz2").style.visibility = "hidden";
 document.getElementById("quiz3").style.visibility = "hidden";
 var covid_check = 0;
 
-var messages = ["You do not show symptoms of COVID-19. Please continue to social distance", "You show symptoms of COVID-19. Please get tested as soon as possible and continue to self-isolate"];
+var messages = ["You do not show symptoms of COVID-19. Please continue to social distance.", "You show symptoms of COVID-19. Please get tested as soon as possible and continue to self-isolate."];
 var covid_result;
 
 function check() {
 
-	document.getElementById("quiz1").style.visibility = "hidden";
 	document.getElementById("quiz2").style.visibility = "visible";
 
-	var question1 = document.quiz1.question1.value;
 
 
+	var question1 = document.getElementById("quiz1").getElementsByClassName("mc")
 
-	if (question1 == "difficulty_breathing" || question1 == "Severe_chestpain" || question1 == "waking_up" || question1 == "confused" || question1 == "consciousness") {
-		covid_check++;
+
+	var i;
+	for (i = 0; i < question1.length; i++) {
+		if (question1[i].checked == true) {
+			covid_check++;
+		}
 	}
+
+	if (covid_check > 8) {
+		covid_result = messages[1];
+	} else {
+		covid_result = messages[0];
+	}
+
+	console.log(covid_result);	
+
+
+
+
+	document.getElementById("quiz1").remove();
 
 }
 
@@ -31,20 +47,28 @@ document.getElementById("button2").addEventListener('click', check2)
 
 function check2() {
 
-	document.getElementById("quiz2").style.visibility = "hidden";
-	document.getElementById("quiz3").style.visibility = "visible";
+	
 
-	var question2 = document.quiz2.question2.value;
+	var question2 = document.getElementById("quiz2").getElementsByClassName("mc")
 
-
-
-	if (question2 == "difficulty_breathing" || question1 == "Severe_chestpain" || question1 == "waking_up" || question1 == "confused" || question1 == "consciousness") {
-		covid_check++;
+	for (i = 0; i < question2.length; i++) {
+		if (question2[i].checked == true) {
+			covid_check++;
+		}
 	}
 
+	if (covid_check > 8) {
+		covid_result = messages[1];
+	} else {
+		covid_result = messages[0];
+	}
+
+	console.log(covid_result);	
 
 
-} 
+	document.getElementById("quiz2").remove();
+	document.getElementById("quiz3").style.visibility = "visible";
+}
 
 
 
@@ -52,26 +76,34 @@ document.getElementById("button3").addEventListener('click', check3)
 
 function check3() {
 
-	document.getElementById("quiz3").style.visibility = "hidden";
 	
-	var question3 = document.quiz3.question3.value;
 
 
 
-	if (question3 == "difficulty_breathing" || question1 == "Severe_chestpain" || question1 == "waking_up" || question1 == "confused" || question1 == "consciousness") {
-		covid_check++;
+
+	var question3 = document.getElementById("quiz3").getElementsByClassName("mc");
+
+	for (i = 0; i < question3.length; i++) {
+		if (question3[i].checked == true) {
+			covid_check++;
+		}
 	}
 
-
-	if (covid_check == 0) {
-		covid_result = messages[0]
-
+	if (covid_check > 8) {
+		covid_result = messages[1];
+	} else {
+		covid_result = messages[0];
 	}
-
+	console.log(covid_result);	
 
 	document.getElementById("after_submit").style.visibility = "visible";
-	document.getElementById("message").innerHTML = "Hello";
 
-	
+	document.getElementById("message").innerHTML = covid_result;	
 
-} 
+	document.getElementById("quiz3").remove();
+
+
+}
+
+
+
